554b650bcbaf36fc5130bef31ff905ab1c998dac
Anjan Pajiyar <130601195+coderanjan@users.noreply.github.com>
2026-06-08 06:04:27 +0000

    modules

--- HELLO-World/hello.js
+++ HELLO-World/hello.js
@@ -1,2 +1,9 @@
-console.log("Hey there! I am 35");
+// const math = require("http")
+// const math = require("fs")
+// const math = require("crypto")
+// const math = require('./math')
+// const {add,sub} = require('./math')
 
+// console.log(add(2,4));
+// console.log(sub(2,4));
+console.log(math);
---
+++ HELLO-World/math.js
@@ -0,0 +1,20 @@
+// function add(a,b){
+//     return a+b
+// }
+
+// function sub(a,b){
+//     return a-b
+// }
+
+// module.exports =add
+// module.exports =sub
+
+// module.exports={
+//     add,
+//      sub,
+//     // addFn : add,
+//     // subFn : sub,
+// }
+
+exports.add = (a,b)=> a+b 
+exports.sub = (a,b)=> a-b 
