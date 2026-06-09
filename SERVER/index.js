10924e34dc139c0bf36f5c803f36b4174dba6d1b
Anjan Pajiyar <130601195+coderanjan@users.noreply.github.com>
2026-06-09 14:11:45 +0000

    handling url in nodejs

--- SERVER/index.js
+++ SERVER/index.js
@@ -1,26 +1,39 @@
 const http = require('http')
 const fs = require('fs')
+const url = require('url')
 
-const myServer = http.createServer((req,res)=>{
-    // console.log('New Rquest Req Rec.');
-    // console.log(req);
-    const log =`${Date.now() }:${req.url} New Req Received\n `
-    fs.appendFile('log.txt',log,(err,data)=>{
-        switch (req.url) {
+const myServer = http.createServer((req, res) => {
+
+    if (req.url === '/favicon.ico') {
+        return res.end()
+    }
+
+    const log = `${Date.now()}:${req.url} New Req Received\n`
+
+    const myUrl = url.parse(req.url,true)
+    console.log(myUrl)
+
+    fs.appendFile('log.txt', log, (err, data) => {
+
+        switch (myUrl.pathname) {
+
             case '/':
                 res.end('homepage')
-                break;
-            case '/about':
-                res.end('I Am Anjan Pajiyar')
                 break
+
+            case '/about':
+                const username=myUrl.query.myname
+                // res.end('I Am Anjan Pajiyar')
+                res.end(`Hi, ${username}`)
+                break
+
             default:
                 res.end('404 Not Found')
+
         }
-        
+
     })
-   
-}) 
 
-myServer.listen(8000,()=> console.log('server started')
-)
+})
 
+myServer.listen(8000, () => console.log('server started'))
