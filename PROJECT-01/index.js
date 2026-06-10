b47639fad191fa5d27b7e98000b21f61244403b1
Anjan Pajiyar <130601195+coderanjan@users.noreply.github.com>
2026-06-10 11:15:27 +0000

    introdunction to postman for rest api's

--- PROJECT-01/index.js
+++ PROJECT-01/index.js
@@ -1,66 +1,79 @@
-const express =require('express')
-const Users = require('./MOCK_DATA.json')
-const app = express()
-const PORT = 8000
+const express = require('express');
+const fs=require('fs')
+const Users = require('./MOCK_DATA.json');
+
+const app = express();
+const PORT = 8000;
 
 
-app.get('/users',(req,res)=>{
+
+app.get('/users', (req, res) => {
     const html = `
-    <ul>
-        ${Users.map((users)=> `<li>${users.first_name}</li>`).join("")}
-    </ul>
-    `
-    res.send(html)
-})
+        <ul>
+            ${Users.map((users) => `<li>${users.first_name}</li>`).join("")}
+        </ul>
+    `;
+
+    res.send(html);
+});
+
+// Middleware - Plugin
+app.use(express.urlencoded({extended:false}))
 
 // Routes
-
-
-app.get('/api/users',(req,res)=>{
-    return res.json(Users)
-})
+app.get('/api/users', (req, res) => {
+    return res.json(Users);
+});
 
 app
-    .routes('/api/users/:id')
-    .get((req,res)=>{
-        const id= Number(req.params.id)
-        const user = Users.find(user => user.id ===id)
+    .route('/api/users/:id')
+    .get((req, res) => {
+        const id = Number(req.params.id);
+        const user = Users.find(user => user.id === id);
 
-        return res.json(user)
+        return res.json(user);
     })
 
-    .patch('/api/users/:id',(req,res)=>{
-    // TODO: Edit the user with id
-    return res.json({status:"pending"})
+    .patch((req, res) => {
+        // TODO: Edit the user with id
+        return res.json({ status: "pending" });
     })
 
-    .delete('/api/users/:id',(req,res)=>{
-    // TODO: Delete the user with id
-    return res.json({status:"pending"})
-    })
-    
+    .delete((req, res) => {
+        // TODO: Delete the user with id
+        return res.json({ status: "pending" });
+    });
 
-// app.get('/api/users/:id',(req,res)=>{
-//     const id= Number(req.params.id)
-//     const user = Users.find(user => user.id ===id)
-
-//     return res.json(user)
-// })
-
-app.post('/api/users',(req,res)=>{
+app.post('/api/users', (req, res) => {
     // TODO: Create new User
-    return res.json({status:"pending"})
+    const body = req.body
+    // console.log("Body",body);
+    Users.push({...body,id:Users.length+1})
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(Users),(err,data)=>{
+         return res.json({ status: "pending" , id :Users.length});
+    })
+
 })
+    
+   
 
-// app.patch('/api/users/:id',(req,res)=>{
+// app.get('/api/users/:id', (req, res) => {
+//     const id = Number(req.params.id);
+//     const user = Users.find(user => user.id === id);
+
+//     return res.json(user);
+// });
+
+// app.patch('/api/users/:id', (req, res) => {
 //     // TODO: Edit the user with id
-//     return res.json({status:"pending"})
-// })
+//     return res.json({ status: "pending" });
+// });
 
-// app.delete('/api/users/:id',(req,res)=>{
+// app.delete('/api/users/:id', (req, res) => {
 //     // TODO: Delete the user with id
-//     return res.json({status:"pending"})
-// })
+//     return res.json({ status: "pending" });
+// });
 
-app.listen(PORT,()=>console.log(`Server Started at PORT:${PORT}`)
-)
+app.listen(PORT, () =>
+    console.log(`Server Started at PORT:${PORT}`)
+);
